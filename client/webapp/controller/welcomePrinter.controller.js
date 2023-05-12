89b6e86e205e623a630fe45ed1d777016760fdbf
Harshvardhan sisodia <harsh@soyuztechnologies.com>
2023-05-12 15:50:06 +0530

    Side Navigation

--- client/webapp/controller/welcomePrinter.controller.js
+++ client/webapp/controller/welcomePrinter.controller.js
@@ -14,6 +14,7 @@ sap.ui.define([
 			this.getModel("appView").setProperty("/layout", "OneColumn");
 			this.getModel("appView").setProperty("/visibleHeader",true);
 			this.getModel("appView").setProperty("/visibility", true);
+			this.getModel("appView").setProperty("/logoutVisibility", true);
 			this.getModel("appView").updateBindings();
 		},
 		onListItemPress:function(){
